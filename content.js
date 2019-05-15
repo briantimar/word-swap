92eefa27706435fc2c306d7331e0c5301bf73e9e
Brian Timar <timarbrian@gmail.com>
2019-05-14 20:49:10 -0700

    cheese -> nuts

--- content.js
+++ content.js
@@ -1,1 +1,16 @@
-console.log("Hello!")
+console.log("Hello!");
+
+function walk(elem) {
+    if (elem.hasChildNodes()) {
+    for (let child of elem.childNodes) {
+        walk(child);
+        };
+    }
+    else {
+        let raw = elem.textContent;
+        let replaced = raw.replace(/cheese/gi, "nuts");
+        elem.textContent = replaced;
+    };
+}
+
+walk(document.body);
