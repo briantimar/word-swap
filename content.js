af459ac1f3f4488c0056357001d47ff916c7e618
Brian Timar <timarbrian@gmail.com>
2019-05-14 21:14:49 -0700

    cheese2meat

--- content.js
+++ content.js
@@ -1,5 +1,23 @@
 console.log("Hello!");
 
+function isUpperCase(s) {
+    return ( s === s.toUpperCase());
+}
+
+function capitalize(s) {
+    return s[0].toUpperCase() + s.slice(1, s.length)
+}
+
+let OLD_LOWER = "cheese";
+let NEW_LOWER = "meat";
+
+let OLD_CAP = capitalize(OLD_LOWER);
+let NEW_CAP = capitalize(NEW_LOWER);
+
+// OLD_LOWER = "\b" + OLD_LOWER + "\b";
+// NEW_LOWER = "\b" + NEW_LOWER + "\b";
+
+
 function walk(elem) {
     if (elem.hasChildNodes()) {
     for (let child of elem.childNodes) {
@@ -8,7 +26,10 @@ function walk(elem) {
     }
     else {
         let raw = elem.textContent;
-        let replaced = raw.replace(/cheese/gi, "nuts");
+        let re_lower = new RegExp(OLD_LOWER, "g");
+        let re_cap = new RegExp(OLD_CAP, "g")
+        let replaced = raw.replace(re_lower, NEW_LOWER);
+        replaced = replaced.replace(re_cap, NEW_CAP);
         elem.textContent = replaced;
     };
 }
